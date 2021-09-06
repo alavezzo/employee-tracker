9aab8155937e1637f419c8f3ea2f6a08a3385825
bowdenshot <anthonyelavezzo@gmail.com>
2021-09-06 01:14:53 -0500

    added task instructions

--- queries/sql.js
+++ queries/sql.js
@@ -131,7 +131,7 @@ function init() {
         } else if ( start === 'View All Roles') {
             allRoles()
         } else if ( start === 'View All Employees') {
-            allEmployess()
+            allEmployees()
         }
     })
 }
@@ -153,7 +153,7 @@ function allRoles() {
         });
     };
 
-function allEmployess() {
+function allEmployees() {
     db.promise().query("SELECT employees.first_name, employees.last_name, roles.title, departments.name AS department, roles.salary, CONCAT_WS(' ', IFNULL(manager.first_name,''), IFNULL(manager.last_name,'')) AS manager FROM employees LEFT JOIN roles ON employees.role_id = roles.id LEFT JOIN departments ON roles.department_id = departments.id LEFT JOIN employees AS manager ON employees.manager_id = manager.id")
         .then( ([rows]) => {
             console.log(cTable.getTable(rows));
@@ -168,5 +168,18 @@ function allEmployess() {
 // const addRoles = `INSERT INTO role((title, salary, department_id)) VALUES(${},${},${})`;
 // const addEmployee = `INSERT INTO employee(first_name, last_name, role_id, manager_id) VALUES(${},${},${})`;
 // const updateEmployee = `UPDATE employee SET role_id = ${} WHERE id = ${}`
+
+// GIVEN a command-line application that accepts user input
+// WHEN I start the application
+// WHEN I choose to view all roles
+// THEN I am presented with the job title, role id, the department that role belongs to, and the salary for that role
+// WHEN I choose to add a department
+// THEN I am prompted to enter the name of the department and that department is added to the database
+// WHEN I choose to add a role
+// THEN I am prompted to enter the name, salary, and department for the role and that role is added to the database
+// WHEN I choose to add an employee
+// THEN I am prompted to enter the employee’s first name, last name, role, and manager and that employee is added to the database
+// WHEN I choose to update an employee role
+// THEN I am prompted to select an employee to update and their new role and this information is updated in the database 
 
 module.exports = init;
