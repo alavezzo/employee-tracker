b9c7ff6dbdc4d30fda6c7011bce1e5135d1034e0
bowdenshot <anthonyelavezzo@gmail.com>
2021-09-06 03:32:04 -0500

    capability to add roles

--- queries/sql.js
+++ queries/sql.js
@@ -1,137 +1,36 @@
 const inquirer = require('inquirer')
 const db = require('../config/connection')
 const cTable = require('console.table')
+const { end } = require('../config/connection')
 
-
-function addDepartment() {
-    inquirer
-    .prompt({
-            type: "input",
-            message: "Enter the department name",
-            name: "departmentName",
-            validate: nameInput => {
-                if (nameInput) {
-                    return true;
-                } else {
-                    console.log("Please provide the department name")
-                    return false;
-                }
-           }
-        })
-    };
-
-function addRole() {
-    inquirer
-        .prompt([
-        {
-            type: "input",
-            message: "Enter title of role",
-            name: "roleTitle",
-            validate: titleInput => {
-                if (titleInput) {
-                    return true;
-                } else {
-                    console.log("Please provide the role title")
-                    return false;
-                }
-            },
-        },
-        {
-            type: "input",
-            message: "Enter the starting salary for this role",
-            name: "salary",
-            validate: salaryInput => {
-                if (salaryInput) {
-                    return true;
-                } else {
-                    console.log("Please provide the starting salary")
-                    return false;
-                }
-        
-            }
-        },
-        {
-            type: "list",
-            message: "What department is the role under?",
-            name: "roleDepartment",
-            choices: [],
-        }
-    ])
-};
-
-function addEmployee() {
-    inquirer
-        .prompt([
-        {
-            type: "input",
-            message: "Enter the employee's first name",
-            name: "firstName",
-            validate: nameInput => {
-                if (nameInput) {
-                    return true;
-                } else {
-                    console.log("Please provide the employee's name")
-                    return false;
-                }
-            }
-        },
-        {
-            type: "input",
-            message: "Enter the employee's last name",
-            name: "lastName",
-            validate: nameInput => {
-                if (nameInput) {
-                    return true;
-                } else {
-                    console.log("Please provide the employee's name")
-                    return false;
-                }
-            }
-        },
-        {
-            type: "list",
-            message: "What is the employee's role?",
-            name: "role",
-            choices: []
-        },
-        {
-            type: "list",
-            message: "Who is the employee's manager?",
-            name: "manager",
-            choices: []
-        }
-    ]);
-};
-
-function updateEmployee() {
-    inquirer
-        .prompt([
-        {
-            type: ''
-
-        },
-        {
-            type: ''
-        }
-    ])
-}
-
-function init() {
-    inquirer
-        .prompt([
+function promptUser() {
+    return inquirer.prompt(
         {
             type: "list",
             message: "What would you like to do?",
             name: "start",
             choices: ['View All Departments', 'View All Roles', 'View All Employees', 'Add a Department', 'Add a Role', 'Add an Employee', 'Update Employee']
         }
-    ]).then( ({ start }) => {
+    )
+};
+
+function init() {
+    promptUser()
+    .then( ({ start }) => {
         if ( start === 'View All Departments') {
             allDepartments()
         } else if ( start === 'View All Roles') {
             allRoles()
         } else if ( start === 'View All Employees') {
             allEmployees()
+        } else if ( start === 'Add a Department') {
+            addDepartment()
+        } else if ( start === 'Add a Role') {
+            addRole()
+        } else if ( start === 'Add an Employee') {
+            addEmployee()
+        } else {
+            updateEmployee()
         }
     })
 }
@@ -161,18 +60,155 @@ function allEmployees() {
         });
     };
 
-// const allDepartments = ;
-// const allRoles = 'SELECT * FROM role';
-// const allEmployees = 'SELECT * FROM employee';
-// const addDepartment = `INSERT INTO department(name) VALUES(${})`; 
+function addDepartment() {
+    inquirer
+        .prompt({
+                type: "input",
+                message: "Enter the department name",
+                name: "departmentName",
+                validate: nameInput => {
+                    if (nameInput) {
+                        return true;
+                    } else {
+                        console.log("Please provide the department name")
+                        return false;
+                    }
+               }
+            })
+        .then(({ departmentName }) => {
+            
+            addDepartmentQuery(departmentName);
+        })
+};
+
+function addDepartmentQuery(name) {
+    db.promise().query(`INSERT INTO departments(name) VALUES('${name}')`)
+        .then(() => {
+            console.log(`Added '${name}' to list of departments`);
+            init();
+        }    
+        )
+};
+
+
+function promptRole(departments) {
+        inquirer
+            .prompt([
+                {
+                    type: "input",
+                    message: "Enter title of role",
+                    name: "roleTitle",
+                    validate: titleInput => {
+                        if (titleInput) {
+                            return true;
+                        } else {
+                            console.log("Please provide the role title")
+                            return false;
+                        }
+                    },
+                },
+                {
+                    type: "input",
+                    message: "Enter the starting salary for this role",
+                    name: "salary",
+                    validate: salaryInput => {
+                        if (salaryInput && !isNaN(salaryInput) ) {
+                            return true;
+                        } else if (isNaN(salaryInput)) {
+                            console.log('Please enter a numerical salary')
+                            return false;
+                        } else {
+                            console.log("Please provide the starting salary")
+                            return false;
+                        }
+                
+                    }
+                },
+                {
+                    type: "list",
+                    message: "What department is the role under?",
+                    name: "roleDepartment",
+                    choices: departments,
+                }
+            ])
+            .then( ({ roleTitle, salary, roleDepartment }) => {
+                let departmentId = 1 + departments.findIndex(x => x === roleDepartment)
+                addRoleQuery(roleTitle, salary, departmentId)
+            })
+    };
+
+function addRole() {
+    db.promise().query({sql: "SELECT departments.name FROM departments", rowsAsArray: true})
+        .then( ([rows]) => {
+            const departments = rows.flat()
+            return departments
+        }).then(departments => {
+            promptRole(departments)
+        })
+}
+
+function addRoleQuery(title, salary, department) {
+    db.promise().query(`INSERT INTO roles(title, salary, department_id) VALUES('${title}',${salary},${department})`)
+        .then(() => {
+            console.log(`Added '${title}' to list of roles`);
+            init();
+        }    
+        )
+}
+    
+function addEmployee() {
+    inquirer
+        .prompt([
+            {
+                type: "input",
+                message: "Enter the employee's first name",
+                name: "firstName",
+                validate: nameInput => {
+                    if (nameInput) {
+                        return true;
+                    } else {
+                        console.log("Please provide the employee's name")
+                        return false;
+                    }
+                }
+            },
+            {
+                type: "input",
+                message: "Enter the employee's last name",
+                name: "lastName",
+                validate: nameInput => {
+                    if (nameInput) {
+                        return true;
+                    } else {
+                        console.log("Please provide the employee's name")
+                        return false;
+                    }
+                }
+            },
+            {
+                type: "list",
+                message: "What is the employee's role?",
+                name: "role",
+                choices: []
+            },
+            {
+                type: "list",
+                message: "Who is the employee's manager?",
+                name: "manager",
+                choices: []
+            }
+        ]);
+    };
+
+function updateEmployee() {
+  
+    }
+
+
 // const addRoles = `INSERT INTO role((title, salary, department_id)) VALUES(${},${},${})`;
 // const addEmployee = `INSERT INTO employee(first_name, last_name, role_id, manager_id) VALUES(${},${},${})`;
 // const updateEmployee = `UPDATE employee SET role_id = ${} WHERE id = ${}`
 
-// GIVEN a command-line application that accepts user input
-// WHEN I start the application
-// WHEN I choose to view all roles
-// THEN I am presented with the job title, role id, the department that role belongs to, and the salary for that role
 // WHEN I choose to add a department
 // THEN I am prompted to enter the name of the department and that department is added to the database
 // WHEN I choose to add a role
